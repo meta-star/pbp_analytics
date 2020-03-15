1aa42bc9f378aef78bf12fe9bc884651fbe0afe6
SuperSonic <supersonic@livemail.tw>
2020-03-15 10:55:03 +0800

    Update

---
+++ libs/comparer/target/page_view.py
@@ -0,0 +1,127 @@
+import os
+import subprocess
+
+import cv2
+import skimage.measure as measure
+
+"""
+    Copyright (c) 2019 SuperSonic(https://randychen.tk)
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+
+# Browser Simulation
+
+class BrowserRender:
+    """
+    The main solution
+    To render web page from QT Webkit with python-webkit2png.
+    But we plan using Gecko/Servo to replace someday.
+    """
+
+    def __init__(self):
+        pass
+
+
+class BrowserAgent:
+    """
+    As a backup solution
+    The class will allow you to use your browser as the agent to take a screenshot form it.
+    """
+
+    def __init__(self, using):
+        if using == "firefox":
+            self.exec_path = "/usr/bin/firefox"
+            self.capture_com = "--screenshot {path} {url} --window-size={size}"
+        elif using == "chrome":
+            self.exec_path = "/usr/bin/google-chrome"
+            self.capture_com = "--headless --screenshot={path} {url} --window-size={size} --hide-scrollbars"
+        elif using == "chromium":
+            self.exec_path = "/usr/bin/chromium-browser"
+            self.capture_com = "--headless --screenshot={path} {url} --window-size={size}"
+        else:
+            assert using is dict, "Configure Error"
+            self.exec_path = using["exec_path"]
+            self.capture_com = using["capture_com"]
+
+    def set_path(self, path):
+        """
+        To setup your browser path in your computer.
+        :param path: Your browser path.
+        :return: bool
+        """
+        if os.path.exists(path):
+            self.exec_path = path
+            return True
+        else:
+            return False
+
+    def capture(self, url, path, size="1920,1080"):
+        capture_com = self.capture_com.format(**{"url": url, "path": path, "size": size})
+        return subprocess.run([self.exec_path, capture_com], shell=True, check=True)
+
+
+# Capture
+
+class WebCapture:
+    """
+    To take screenshot for PBP.
+    """
+
+    def __init__(self, config):
+        self.cache_path = config
+        self.compare_path = config
+
+        self.browser = config
+
+        if not os.path.exists(self.cache_path):
+            os.mkdir(self.cache_path)
+        if not os.path.exists(self.compare_path):
+            os.mkdir(self.compare_path)
+
+    @staticmethod
+    def __set_browser_simulation(type_id):
+        """
+        Set Browser Simulation by ID
+        :param type_id: Type ID
+        :return: class object
+        """
+        return {
+            1: BrowserRender,
+            2: BrowserAgent
+        }[type_id]
+
+    def get_page_image(self, target_url, output_image='out.png', agent="PC"):
+        """
+        To get the image of the URL you provided.
+        :param target_url: The target URL
+        :param output_image: Output path (optional)
+        :param agent: PC or Mobile as user-agent, it will not work if you are using BrowserAgent (optional)
+        :return: bool
+        """
+        if os.path.isfile(self.cache_path + output_image):
+            os.remove(self.cache_path + output_image)
+        self.browser.capture(target_url, self.cache_path + output_image)
+        return True
+
+    @staticmethod
+    def image_object(path):
+        """
+        Create NumPy Array by OpenCV
+        :param path: The Image Path
+        :return: Image object
+        """
+        return cv2.imread(path, 0)
+
+    @staticmethod
+    def image_compare(img1, img2):
+        """
+        To compare image using structural similarity index
+        :param img1: Image object
+        :param img2: Image object
+        :return: float of the similar lever
+        """
+        return measure.compare_ssim(img1, img2, multichannel=True)
