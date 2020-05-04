51cbca00f9989841879b6c4d3de885c4c4dd6d28
SuperSonic <supersonic@livemail.tw>
2020-05-04 19:39:58 +0800

    Trying for aiomysql

--- libs/data.py
+++ libs/data.py
@@ -1,6 +1,7 @@
 from functools import wraps
+from typing import Callable
 
-import mysql.connector as sql_client
+import aiomysql
 
 """
     Copyright (c) 2020 Star Inc.(https://starinc.xyz)
@@ -19,14 +20,13 @@ def mysql_checker(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
         try:
-            result = function(*args, **kwargs)
+            result = args[0].connect(function, (args, kwargs,))
             args[0].db_error_checkpoint = 0
             return result
-        except sql_client.errors.OperationalError:
+        except aiomysql.OperationalError:
             if args[0].db_error_checkpoint:
                 assert "sql_error"
             args[0].db_error_checkpoint = 1
-            args[0].__init__(args[0].handle)
             return function(*args)
 
     return wrapper
@@ -35,171 +35,154 @@ def mysql_checker(function):
 class Data:
     """
     To control MySQL for PBP
+
+    Attributes
+    -----------
+    handle: Analytics object
     """
 
     db_error_checkpoint = 0
 
     def __init__(self, pbp_handle):
-        """
-        Configure and initialize database details
-
-        :param pbp_handle: Analytics object
-        """
         self.handle = pbp_handle
-        self.db_client = sql_client.connect(**pbp_handle.cfg["MySQL"])
+
+    async def connect(self, function: Callable, *args, **kwargs):
+        pool = await aiomysql.create_pool(**self.handle.cfg["MySQL"])
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                function(cur, *args, **kwargs)
+        pool.close()
+        await pool.wait_closed()
 
     @mysql_checker
-    def check_trustlist(self, url: str):
+    def check_trustlist(self, cursor: aiomysql.cursors, url: str):
         """
         To check URL whether exists in trustlist
 
         :param url: URL
+        :param cursor:
         :return: string of UUID or NoneType
         """
-        cursor = self.db_client.cursor(dictionary=True)
         cursor.execute(
             "SELECT `uuid` FROM `trustlist` WHERE `url` = %s",
             (url,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0]
 
     @mysql_checker
-    def check_trust_domain(self, domain: str):
+    def check_trust_domain(self, cursor: aiomysql.cursors, domain: str):
         """
         To check URL whether exists in trust_domain list
 
         :param domain: domain
         :return: string of UUID or NoneType
         """
-        cursor = self.db_client.cursor(dictionary=True)
         cursor.execute(
             "SELECT `uuid` FROM `trust_domain` WHERE `domain` = %s",
             (domain,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0]
 
     @mysql_checker
-    def check_blacklist(self, url: str):
+    def check_blacklist(self, cursor: aiomysql.cursors, url: str):
         """
         To check URL whether exists in blacklist
 
         :param url: URL
         :return: dict of URL and Mark-Date or NoneType
         """
-        cursor = self.db_client.cursor(dictionary=True)
         cursor.execute(
             "SELECT `url`, `date` FROM `blacklist` WHERE `url` = %s",
             (url,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0]
 
     @mysql_checker
-    def check_warnlist(self, url: str):
+    def check_warnlist(self, cursor: aiomysql.cursors, url: str):
         """
         To check URL whether exists in warnlist
 
         :param url: URL
         :return: dict of URL, similar URL and Mark-Date or NoneType
         """
-        cursor = self.db_client.cursor(dictionary=True)
         cursor.execute(
             "SELECT `url`, `origin`, `date` FROM `warnlist` WHERE `url` = %s",
             (url,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0]
 
     @mysql_checker
-    def get_urls_from_trustlist(self):
+    def get_urls_from_trustlist(self, cursor: aiomysql.cursors):
         """
         Fetch all URL in trustlist
 
         :return: list of URL
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "SELECT `url` FROM `trustlist`"
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return [record[0] for record in result]
         return []
 
     @mysql_checker
-    def get_view_narray_from_trustlist(self):
+    def get_view_narray_from_trustlist(self, cursor: aiomysql.cursors):
         """
         Fetch all target_view_narray in trustlist
 
         :return: dict of URL and NumPy Array
         """
-        cursor = self.db_client.cursor(dictionary=True)
         cursor.execute(
             "SELECT `url`, `target_view_narray` FROM `trustlist`"
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         return result
 
     @mysql_checker
-    def find_page_by_view_signature(self, signature: str):
+    def find_page_by_view_signature(self, cursor: aiomysql.cursors, signature: str):
         """
         Search URL by view_signature in trustlist
 
         :param signature: string hashed
         :return: URL or NoneType
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "SELECT `url` FROM `trustlist` WHERE `target_view_signature` = %s",
             (signature,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0][0]
         return None
 
     @mysql_checker
-    def find_result_cache_by_url_hash(self, url_hash: str):
+    def find_result_cache_by_url_hash(self, cursor: aiomysql.cursors, url_hash: str):
         """
         Search cache by url_hash in result_cache
 
         :param url_hash: URL hashed
         :return: float of the-trust-score or NoneType
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "SELECT `score` FROM `result_cache` WHERE `url_hash` = %s",
             (url_hash,)
         )
         result = cursor.fetchall()
-        self.db_client.commit()
-        cursor.close()
         if result:
             return result[0][0]
         return None
 
     @mysql_checker
-    def upload_result_cache(self, url_hash: str, score: float):
+    def upload_result_cache(self, cursor: aiomysql.cursors, url_hash: str, score: float):
         """
         Upload the-trust-score to cache
 
@@ -207,30 +190,24 @@ class Data:
         :param score: float of the-trust-score
         :return:
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "INSERT INTO `result_cache`(`url_hash`, `score`) VALUES (%s, %s)",
             (url_hash, score)
         )
-        self.db_client.commit()
-        cursor.close()
         return True
 
     @mysql_checker
-    def clean_result_cache(self):
+    def clean_result_cache(self, cursor: aiomysql.cursors):
         """
         Clean result caches
 
         :return: True
         """
-        cursor = self.db_client.cursor()
         cursor.execute("TRUNCATE TABLE `result_cache`")
-        self.db_client.commit()
-        cursor.close()
         return True
 
     @mysql_checker
-    def upload_view_sample(self, url: str, view_signature: str, view_data: str):
+    def upload_view_sample(self, cursor: aiomysql.cursors, url: str, view_signature: str, view_data: str):
         """
         Upload ViewSample for PageView
 
@@ -239,7 +216,6 @@ class Data:
         :param view_data: string of num array base64 encoded
         :return: True
         """
-        cursor = self.db_client.cursor()
         if self.check_trustlist(url):
             cursor.execute(
                 "UPDATE `trustlist` SET `target_view_signature` = %s, `target_view_narray` = %s WHERE `url` = %s",
@@ -252,29 +228,24 @@ class Data:
                 "VALUES (UUID(), %s, %s, %s)",
                 (url, view_signature, view_data)
             )
-        self.db_client.commit()
-        cursor.close()
         return True
 
     @mysql_checker
-    def mark_as_blacklist(self, url: str):
+    def mark_as_blacklist(self, cursor: aiomysql.cursors, url: str):
         """
         Mark URL to blacklist by Database
 
         :param url: URL to mark
         :return: True
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "INSERT INTO `blacklist`(`uuid`, `url`, `date`) VALUES (UUID(), %s, NOW())",
             (url,)
         )
-        self.db_client.commit()
-        cursor.close()
         return True
 
     @mysql_checker
-    def mark_as_warnlist(self, url: str, origin_url: str):
+    def mark_as_warnlist(self, cursor: aiomysql.cursors, url: str, origin_url: str):
         """
         Mark URL to warnlist by PageView
 
@@ -282,11 +253,8 @@ class Data:
         :param origin_url: the URL similar to
         :return: True
         """
-        cursor = self.db_client.cursor()
         cursor.execute(
             "INSERT INTO `warnlist`(`uuid`, `url`, `origin`, `date`) VALUES (UUID(), %s, %s, NOW())",
             (url, origin_url)
         )
-        self.db_client.commit()
-        cursor.close()
         return True
